1f1a95b51644894f760115d96644a86696921d4e
Steven L <steven.leibrock@gmail.com>
2020-04-03 16:25:08 -0400

    Cleaning up crate lib defs, updating to v0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub mod launchpad;
+// lib.rs
+// includes the details of the crate's module space
+
 pub mod colors;
 pub mod utils;
 pub mod devices;
 pub mod aliases;
 pub mod traits;
-
-pub use launchpad::*;
-pub use colors::*;
-pub use devices::*;
-pub use aliases::*;
-pub use traits::*;
-
 pub mod prelude;
+
+// end lib.rs
